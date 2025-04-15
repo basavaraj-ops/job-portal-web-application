57e493614f0058e8901ebf606e7303283da5b3a9
basavaraj-ops <sagarbasu21@gmail.com>
2025-04-15 15:36:59 +0530

    updated app

--- backend/app.py
+++ backend/app.py
@@ -400,6 +400,7 @@ def search():
 
 # Run the app
 if __name__ == '__main__':
-     port = int(os.environ.get('PORT', 5000))
-     app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True)
+    
+     
 
